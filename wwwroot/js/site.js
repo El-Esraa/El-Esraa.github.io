3b5709ddcc51ffe89d5a6095fe2c6d38015dcd0c
El-Esraa <106170743+El-Esraa@users.noreply.github.com>
2022-05-24 17:52:20 +0200

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,57 @@
+//Responsive Navbar
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+var slideIndex = 1;
+//showSlides(slideIndex);
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+setInterval(() => {plusSlides(1);}, 3000);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//google maps
+function initMap() {
+//30.1572659944518, 31.313910298484046
+  // The location of Uluru
+  const uluru = { lat:30.1572659944518, lng: 31.313910298484046 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: uluru,
+  });
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
